5e2f68b91e5508fa30ab0c5477b69fade2c73318
Lunka229 <lyuba.zhar1756@gmail.com>
2026-08-16 15:39:25 +0300

    Перенос конфигурации AI в переменные среды

--- src/config.py
+++ src/config.py
@@ -4,5 +4,10 @@ from dotenv import load_dotenv
 load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
+AI_MODEL=os.getenv(
+    "AI_MODEL",
+    "qwen3:4b"
+)
+
 if not BOT_TOKEN:
     raise ValueError("BOT_TOKEN не найден")
--- src/services/ollama_client.py
+++ src/services/ollama_client.py
@@ -1,11 +1,12 @@
 import ollama
+from src.config import AI_MODEL
 
 async def generate_response(
         promt:str,
         model:str = "qwen3:4b" 
 ) -> str:
     response = ollama.chat(
-        model=model,
+        model=AI_MODEL,
         messages=[
             {
                 "role":"user",
